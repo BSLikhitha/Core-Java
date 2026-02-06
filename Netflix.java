04c5b938aeb87dc9aeed5134e9f60dfe5bee5a26
likhitha <likhithabs03@gmail.com>
2026-02-06 12:35:20 +0530

    array java files

--- Netflix.java
+++ Netflix.java
@@ -1,10 +1,48 @@
 class Netflix{
 	public static void main(String[] args){
-		String KannadaMoives[] = {"Kantara", "KGF Chapter 1", "KGF Chapter 2", "Avane Srimannarayana", "RangiTaranga", "Dia", "French Biriyani", "Kavaludaari", "Popcorn Monkey Tiger", "Ugramm", "Mayabazar 2016", "Birbal", "Gantumoote", "Chamak", "Shivaji Surathkal", "Hero", "Law", "Sakath", "Rudri", "Maya"};
-		String MalayalamMoives[] = {"Minnal Murali", "Jana Gana Mana", "Nayattu", "Kurup", "Uyare", "Lucifer", "Drishyam", "Drishyam 2", "Kumbalangi Nights", "Android Kunjappan", "Forensic", "Take Off", "Sudani from Nigeria", "Operation Java", "Cold Case", "Home", "Helen", "The Priest", "Vikruthi", "Tharangam"};
-		String HindiMovies[] = {"RRR Hindi", "Dangal", "Andhadhun", "Article 15", "Bulbbul", "Ludo", "Gunjan Saxena", "Pagglait", "Mission Majnu", "Jaane Jaan", "Monica O My Darling", "Khufiya", "Do Patti", "Jawan", "Animal", "Raazi", "Badla", "Talvar", "Shershaah", "Delhi Crime Movie"};
-		String TamilMovies[] = {"Leo", "Master", "Jagame Thandhiram", "Asuran", "Vikram Vedha", "Super Deluxe", "Kaithi", "Doctor", "Beast", "Valimai", "Karnan", "Sillu Karuppatti", "Paava Kadhaigal", "Tughlaq Durbar", "Mahaan", "Thunivu", "Jigarthanda", "96", "Soorarai Pottru", "Visaranai"};
-		String TelgueMovies[] = {"RRR", "Baahubali The Beginning", "Baahubali 2 The Conclusion", "Dasara", "Hi Nanna", "Salaar", "Rangasthalam", "Pushpa The Rise", "Ala Vaikunthapurramuloo", "Sita Ramam", "Jersey", "Eega", "Maharshi", "Vakeel Saab", "Brochevarevarura", "Dear Comrade", "Bichagadu", "Leader", "Mathu Vadalara", "Gamyam"};
+		String KannadaMoives[] = {"Kantara", "KGF Chapter 1", "KGF Chapter 2", "Avane Srimannarayana", "RangiTaranga",
+								  "Dia", "French Biriyani", "Kavaludaari", "Popcorn Monkey Tiger", "Ugramm", "Mayabazar 2016", "Birbal", "Gantumoote", 
+								  "Chamak", "Shivaji Surathkal", "Hero", "Law", "Sakath", "Rudri", "Maya"};
+		for(String kannada:KannadaMoives)
+		{
+			System.out.println(kannada);
+		}
+		System.out.println("---------------");
+		
+		String MalayalamMoives[] = {"Minnal Murali", "Jana Gana Mana", "Nayattu", "Kurup", "Uyare", "Lucifer", "Drishyam", 
+									"Drishyam 2", "Kumbalangi Nights", "Android Kunjappan", "Forensic", "Take Off", "Sudani from Nigeria", "Operation Java", 
+									"Cold Case", "Home", "Helen", "The Priest", "Vikruthi", "Tharangam"};
+		for(String malayalam:MalayalamMoives)
+		{
+			System.out.println(malayalam);
+		}
+		System.out.println("---------------");
+		
+		String HindiMovies[] = {"RRR Hindi", "Dangal", "Andhadhun", "Article 15", "Bulbbul", "Ludo", "Gunjan Saxena", "Pagglait",
+								"Mission Majnu", "Jaane Jaan", "Monica O My Darling", "Khufiya", "Do Patti", "Jawan", "Animal", "Raazi", "Badla", "Talvar", 
+								"Shershaah", "Delhi Crime Movie"};
+		for(String hindi:HindiMovies)
+		{
+			System.out.println(hindi);
+		}
+		System.out.println("---------------");
+		
+		String TamilMovies[] = {"Leo", "Master", "Jagame Thandhiram", "Asuran", "Vikram Vedha", "Super Deluxe", "Kaithi", "Doctor",
+								"Beast", "Valimai", "Karnan", "Sillu Karuppatti", "Paava Kadhaigal", "Tughlaq Durbar", "Mahaan", "Thunivu", "Jigarthanda", 
+								"96", "Soorarai Pottru", "Visaranai"};
+		for(String tamil:TamilMovies)
+		{
+			System.out.println(tamil);
+		}
+		System.out.println("---------------");
+		
+		String TelgueMovies[] = {"RRR", "Baahubali The Beginning", "Baahubali 2 The Conclusion", "Dasara", "Hi Nanna", "Salaar",
+								 "Rangasthalam", "Pushpa The Rise", "Ala Vaikunthapurramuloo", "Sita Ramam", "Jersey", "Eega", "Maharshi", "Vakeel Saab", 
+								 "Brochevarevarura", "Dear Comrade", "Bichagadu", "Leader", "Mathu Vadalara", "Gamyam"};
+		for(String teglue:TelgueMovies)
+		{
+			System.out.println(teglue);
+		}
 	}
 }
 		
